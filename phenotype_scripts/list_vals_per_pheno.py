08efde7b94793f1adaf6bbadf061de2f17348f89
Andreas Karwath <karwath@uni-mainz.de>
2017-04-01 09:06:04 +0200

    added UTF-8, some counting for entries and some questions; added dir_* as in other scripts

--- phenotype_scripts/list_vals_per_pheno.py
+++ phenotype_scripts/list_vals_per_pheno.py
@@ -1,22 +1,55 @@
 import csv
 import sys
+import os.path
+
+debug = False
+
+def incHash(theHash,theKey):
+    try:
+        theHash[theKey]
+    except:
+        theHash[theKey] = 0
+        pass
+    theHash[theKey] += 1
+    
+
 
 def get_vals(pheno_num,pheno_file):
-    with open(pheno_file) as f:
+    with open(pheno_file,  encoding="utf-8") as f:
         r = csv.reader(f,delimiter=';')
         names = next(r)
-        print(list(enumerate(names)))
-        print(names[pheno_num] + "\n")
+        if debug:
+            print(list(enumerate(map(lambda x: x.encode(), names))))
+        # print('Phenotype:%3s:%30s' % (pheno_num, names[pheno_num]))
+        print(names[pheno_num]+'\n')
         d = {}
+        # ak: for later
+        count = {}
+
+        # back to original:
         for row in r:
             d[row[pheno_num].lower()] = 1
+            incHash(count,row[pheno_num].lower())
         for k in sorted(d.keys()):
             print(k + ';')
+
+        # # why not:
+        # # without count
+        # print(';\n'.join(sorted(count.keys())))
+        # # with count
+        # print(';\n'.join(map(lambda x: '%s:%s' % (x[0],x[1]),sorted(zip(count.keys(),count.values())))))
+        # # is that bad style because of map and lambda?
         
     
 
 if __name__ == "__main__":
-    pheno_file = "../data/phenotypes/phenotypes_201604281702.csv"
+    data_dir            = '..'+os.path.sep+'..'+os.path.sep+'data' 
+    data_dir_genotype   = '%s%sgenotypes' % (data_dir,os.path.sep)
+    data_dir_phenotype  = '%s%sphenotypes' % (data_dir,os.path.sep)
+    data_dir_annotation = '%s%sannotation' % (data_dir,os.path.sep)
+
+    pheno_file          = "%s%sphenotypes_201604281702.csv" % (data_dir_phenotype,os.path.sep)
+
     pheno_num = int(sys.argv[1])
     get_vals(pheno_num,pheno_file)
     
