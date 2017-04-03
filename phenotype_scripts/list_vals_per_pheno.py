6ff0e7ee633c2dede70bb9c2d072ad12075e4c11
Andreas Karwath <karwath@uni-mainz.de>
2017-04-03 13:07:32 +0200

    writing to mapping file directly

--- phenotype_scripts/list_vals_per_pheno.py
+++ phenotype_scripts/list_vals_per_pheno.py
@@ -20,6 +20,9 @@ def incHash(theHash,theKey):
 
 
 def get_vals(pheno_num,pheno_file):
+    return_pheno_name = None
+    return_pheno_vals = []
+    
     with open(pheno_file,  encoding="utf-8") as f:
         r = csv.reader(f,delimiter=';')
         names = next(r)
@@ -27,36 +30,36 @@ def get_vals(pheno_num,pheno_file):
             print(list(enumerate(map(lambda x: x.encode(), names))))
         # print('Phenotype:%3s:%30s' % (pheno_num, names[pheno_num]))
         print(names[pheno_num]+'\n')
+        return_pheno_name = names[pheno_num]
         d = {}
-        # ak: for later
-        count = {}
-
-        # back to original:
         for row in r:
             d[row[pheno_num].lower()] = 1
-            incHash(count,row[pheno_num].lower())
-        for k in sorted(d.keys()):
-            # after having problems with unicode again
-            print('%s' % (k.encode(),) + ';')
+        if debug:
+            for k in sorted(d.keys()):
+                # after having problems with unicode again
+                print('%s' % (k.encode(),) + ';')
+                # print(k+';')
     
-        # # why not:
-        # # without count
-        # print(';\n'.join(sorted(count.keys())))
-        # # with count
-        # print(';\n'.join(map(lambda x: '%s:%s' % (x[0].encode(),x[1]),sorted(zip(count.keys(),count.values())))))
-        # # is that bad style because of map and lambda?
-        
+        return_pheno_vals = sorted(d.keys())
+        return (return_pheno_name,return_pheno_vals)
     
 
+def write_vals(pheno_name,pheno_vals,mapping_dir):
+    mapping_file_name = '%s%s%s' % (mapping_dir,os.path.sep,pheno_name)
+    with open(mapping_file_name,  'w', encoding="utf-8") as fh:
+        fh.writelines(map(lambda x: '%s;\n' % (x,), pheno_vals))
+        fh.close()
+    
 if __name__ == "__main__":
     data_dir            = '..'+os.path.sep+'..'+os.path.sep+'data' 
     data_dir_genotype   = '%s%sgenotypes' % (data_dir,os.path.sep)
     data_dir_phenotype  = '%s%sphenotypes' % (data_dir,os.path.sep)
     data_dir_annotation = '%s%sannotation' % (data_dir,os.path.sep)
+    mapping_dir         = "mapping"
 
     #pheno_file          = "%s%sphenotypes_201604281702.csv" % (data_dir_phenotype,os.path.sep)
     pheno_file          = "%s%sphenotypes_201604281702.csv.clean.csv" % (data_dir_phenotype,os.path.sep)
 
     pheno_num = int(sys.argv[1])
-    get_vals(pheno_num,pheno_file)
-    
+    (pheno_name,pheno_vals) = get_vals(pheno_num,pheno_file)
+    write_vals(pheno_name,pheno_vals,mapping_dir)
