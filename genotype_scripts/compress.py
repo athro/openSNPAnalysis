018ee08608b205ad9056f05a00a1bd22c5dfc8af
amandaclare <afc@aber.ac.uk>
2017-04-05 15:34:30 +0200

    Added code to read compressed files

---
+++ genotype_scripts/compress.py
@@ -0,0 +1,48 @@
+import gzip
+import zipfile
+import os
+
+magic = {}
+magic["zip"] = b'\x50\x4b\x03\x04'
+magic["gzip"] = b'\x1f\x8b\x08'
+#magic["bzip"] = b'\x42\x5a\x68'
+
+
+def open_zip(filename):
+    zf = zipfile.ZipFile(filename)
+    filenames = zf.namelist()
+    fh = zf.open(filenames[0])
+    return fh
+
+def open_gzip(filename):
+    fh = gzip.GzipFile(filename)
+    return fh
+
+get_fh = {}
+get_fh["zip"] = open_zip
+get_fh["gzip"] = open_gzip
+get_fh["plain"] = open
+
+
+def compress_open(filename):
+    with open(filename, 'rb') as f:
+        start_of_file = f.read(1024)
+    for (kind, bytes) in magic.items():
+        if start_of_file.startswith(bytes):
+            return get_fh[kind](filename)
+    return open(filename)
+
+get_fh = {}
+get_fh["zip"] = open_zip
+get_fh["gzip"] = open_gzip
+get_fh["plain"] = open
+
+
+if __name__ = "__main__":
+    directory = '../../data/genotypes/'
+    files = os.listdir(directory)
+    for filename in files:
+        try:
+            cf = compress_open(directory+"/"+filename)
+        except:
+            print("Can't uncompress "+filename)
