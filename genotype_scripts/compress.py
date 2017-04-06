55be4bb0e014531a81559e7cc4482f6db9d3389b
Andreas Karwath <karwath@uni-mainz.de>
2017-04-06 07:41:02 +0200

    initial idea for dealing with compressed xls files

--- genotype_scripts/compress.py
+++ genotype_scripts/compress.py
@@ -1,12 +1,36 @@
 import gzip
 import zipfile
 import os
+#AK:TBD:# xls unfinished and untested
+#AK:TBD:import xlrd
 
 magic = {}
 magic["zip"] = b'\x50\x4b\x03\x04'
 magic["gzip"] = b'\x1f\x8b\x08'
 #magic["bzip"] = b'\x42\x5a\x68'
+#AK:TBD:magic["xls"] = b'\xd0\xcf'
 
+
+#AK:TBD:# change to filehanle for zipped xls?
+#AK:TBD:def test_xsl(filename):
+#AK:TBD:    with open(filename, 'rb') as f:
+#AK:TBD:        buffer = f.read(1024)
+#AK:TBD:    return_val = False
+#AK:TBD:    if buffer.startswith(magic["xls"]):
+#AK:TBD:        return_val = True
+#AK:TBD:    return return_val
+        
+
+#AK:TBD:# Idea: open xls, convert it to temp. csv, return filehandle for csv
+#AK:TBD:def open_xls(filename):
+#AK:TBD:    wb = xlrd.open_workbook(filename_temp)
+#AK:TBD:    ws = wb.sheets()[0]
+#AK:TBD:    temp_csv_file_name = get_temp_csv_file_name
+#AK:TBD:    temp_csv_file = open(temp_csv_file_name, 'wb')
+#AK:TBD:    wr = csv.writer(temp_csv_file, quoting=csv.QUOTE_ALL)
+#AK:TBD:    for rownum in xrange(ws.nrows):
+#AK:TBD:        wr.writerow(ws.row_values(rownum))
+#AK:TBD:    temp_csv_file.close()
 
 def open_zip(filename):
     zf = zipfile.ZipFile(filename)
@@ -21,7 +45,9 @@ def open_gzip(filename):
 get_fh = {}
 get_fh["zip"] = open_zip
 get_fh["gzip"] = open_gzip
+#AK:TBD:get_fh["xls"] = open_xls
 get_fh["plain"] = open
+
 
 
 def compress_open(filename):
@@ -35,6 +61,7 @@ def compress_open(filename):
 get_fh = {}
 get_fh["zip"] = open_zip
 get_fh["gzip"] = open_gzip
+#AK:TBD:get_fh["xls"] = open_xls
 get_fh["plain"] = open
 
 
